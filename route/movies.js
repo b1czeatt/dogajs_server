6d65ed3f5d3dd56f5025eff98fb634d9dabeea5e
b1czeatt <144415537+b1czeatt@users.noreply.github.com>
2025-03-10 12:50:45 +0100

    Add files via upload

--- route/movies.js
+++ route/movies.js
@@ -9,8 +9,8 @@ router.get('/:id', moviesController.getMoviesById)
 
 router.post('/', moviesController.createMovie)
 
-router.put('/:id', moviesController)
+router.put('/:id', moviesController.updateMovie)
 
-router.delete('/:id', moviesController)
+router.delete('/:id', moviesController.deleteMovie)
 
 export default router
