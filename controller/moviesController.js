9a471f9538cc6add59552105b6b793ed32a7e163
b1czeatt <144415537+b1czeatt@users.noreply.github.com>
2025-03-10 12:51:23 +0100

    Add files via upload

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -23,3 +23,33 @@ export const createMovie = (req,res) =>
     res.push(movie)
     res.status(200).json(movie)
 }
+
+export const updateMovie = (req, res) =>
+    {
+        const id = req.params.id
+        if (id < 0 || id >= movies.length) {
+            return res.status(404).json({message: 'Movie not found'})
+        }
+        else
+        {
+            const movie = req.body
+            if(!movie.oscar || !book.title || !book.published || !movie.producer) {
+                return res.status(400).json({message: 'Invalid data'})
+            }
+            movies[id] = movie
+            res.status(200).json(movie)
+        }
+    }
+    
+    export const deleteMovie = (req, res) =>
+    {
+        const id = req.params.id
+        if (id < 0 || id >= movies.length) {
+            return res.status(404).json({message: 'Movie not found'})
+        }
+        else
+        {
+            movies.splice(id, 1)
+            res.status(200).json({message: 'Movie deleted'})
+        }
+    }
